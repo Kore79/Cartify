1991be0368ee73a4c62473644606fa0ffdeb7d18
Kore79 <oorekuku@gmail.com>
2024-09-19 13:48:19 +0100

    Minor changes

--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -5,7 +5,6 @@ import { useNavigate, Link } from 'react-router-dom';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import SearchBox from './SearchBox';
-import logo from '../assets/logo.png';
 import { resetCart } from '../slices/cartSlice';
 
 const Header = () => {
@@ -21,8 +20,7 @@ const Header = () => {
     try {
       await logoutApiCall().unwrap();
       dispatch(logout());
-      // NOTE: here we need to reset cart state for when a user logs out so the next
-      // user doesn't inherit the previous users cart and shipping
+      // Reset the cart state when a user logs out
       dispatch(resetCart());
       navigate('/login');
     } catch (err) {
@@ -32,27 +30,40 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar
+        style={{ backgroundColor: '#2C3E50' }} // Updated navbar color
+        variant='dark'
+        expand='lg'
+        collapseOnSelect
+      >
         <Container>
-          <Navbar.Brand as={Link} to='/'>
-            <img src={logo} alt='ProShop' />
-            ProShop
+          <Navbar.Brand as={Link} to='/' style={{ fontSize: '1.8rem', fontWeight: 'bold', color: '#ECF0F1' }}> 
+            Cartify
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
               <SearchBox />
-              <Nav.Link as={Link} to='/cart'>
+              <Nav.Link as={Link} to='/cart' style={{ color: '#ECF0F1' }}>
                 <FaShoppingCart /> Cart
                 {cartItems.length > 0 && (
-                  <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                  <Badge
+                    pill
+                    bg='warning'
+                    text='dark'
+                    style={{ marginLeft: '5px' }}
+                  >
                     {cartItems.reduce((a, c) => a + c.qty, 0)}
                   </Badge>
                 )}
               </Nav.Link>
               {userInfo ? (
                 <>
-                  <NavDropdown title={userInfo.name} id='username'>
+                  <NavDropdown
+                    title={userInfo.name}
+                    id='username'
+                    style={{ color: '#ECF0F1' }}
+                  >
                     <NavDropdown.Item as={Link} to='/profile'>
                       Profile
                     </NavDropdown.Item>
@@ -62,7 +73,7 @@ const Header = () => {
                   </NavDropdown>
                 </>
               ) : (
-                <Nav.Link as={Link} to='/login'>
+                <Nav.Link as={Link} to='/login' style={{ color: '#ECF0F1' }}>
                   <FaUser /> Sign In
                 </Nav.Link>
               )}
